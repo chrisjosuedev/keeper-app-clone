ad4938ab1a583991c4426001b2715234f1b18921
Cristhian Martínez <chrismartinezlara@outlook.es>
2022-09-16 19:11:03 -0600

    MUI Design

--- src/Components/CreateArea.js
+++ src/Components/CreateArea.js
@@ -1,10 +1,15 @@
 import React, { useState } from 'react';
+import Zoom from '@mui/material/Zoom';
+import Fab from '@mui/material/Fab';
+import AddIcon from '@mui/icons-material/Add';
 
 function CreateArea({ onAdd }) {
     const [note, setNote] = useState({
         title: '',
         content: '',
     });
+
+    const [animationState, setAnimationState] = useState(false);
 
     function handleText(e) {
         const { name, value } = e.target;
@@ -16,9 +21,15 @@ function CreateArea({ onAdd }) {
         });
     }
 
+    /** Animation */
+    function handleClick() {
+        setAnimationState(true);
+    }
+
     return (
         <div>
             <form
+                className="create-note"
                 onSubmit={(e) => {
                     onAdd(e, note);
                     setNote({
@@ -27,21 +38,29 @@ function CreateArea({ onAdd }) {
                     });
                 }}
             >
-                <input
-                    onChange={handleText}
-                    name="title"
-                    placeholder="Title"
-                    autoFocus
-                    value={note.title}
-                />
+                {animationState && (
+                    <input
+                        onChange={handleText}
+                        name="title"
+                        placeholder="Title"
+                        autoFocus
+                        value={note.title}
+                    />
+                )}
+
                 <textarea
                     onChange={handleText}
                     name="content"
                     placeholder="Take a note..."
-                    rows="3"
+                    rows={animationState ? '3' : '1'}
+                    onClick={handleClick}
                     value={note.content}
                 />
-                <button type="submit">Add</button>
+                <Zoom in={animationState}>
+                    <Fab type="submit">
+                        <AddIcon />
+                    </Fab>
+                </Zoom>
             </form>
         </div>
     );
--- src/Components/Note.js
+++ src/Components/Note.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 function Note({ title, content, id, onDelete }) {
     return (
         <div className="note">
             <h1>{title}</h1>
             <p>{content}</p>
-            <button onClick={() => onDelete(id)}>DELETE</button>
+            <button onClick={() => onDelete(id)}>
+                <DeleteIcon />
+            </button>
         </div>
     );
 }
